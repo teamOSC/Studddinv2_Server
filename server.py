ef6d6466d881c20ae783d7bd406e2784e9b8399e
sauravtom <stomatrix@gmail.com>
2015-01-24 18:43:25 +0530

    edited feed function

--- server.py
+++ server.py
@@ -82,27 +82,43 @@ def scrapeQuora(category):
     soup = BeautifulSoup( urllib2.urlopen(url).read() ,'xml' )
     arr =[]
     for q in soup.find_all("item"):
+        d = {}
         d['title'] = q.find('title').get_text()
         d['link'] = q.find('link').get_text()
         arr.append(d)
     return arr
 
+@app.route('/feed.json',methods=['GET'])
+def feed():
+    arr = []
+    interests = request.args.get('interests') or ''
+
+    if 'physics' in interests:
+        arr.append(scrapeQuora('Physics'))
+    if 'chemistry' in interests:
+        arr.append(scrapeQuora('Chemistry'))
+    if 'math' in interests:
+        arr.append(scrapeQuora('Mathematics'))
+    if 'biology' in interests:
+        arr.append(scrapeQuora('Biology-1'))
+
+    return json.dumps(arr)
+
 @app.route('/feed',methods=['GET'])
 def feed():
     arr = []
-    categories = request.args.get('categories') or 0
-    category_string = categories.join(',').lower()
-    if 'physics' in category_string:
+    interests = request.args.get('interests') or ''
+
+    if 'physics' in interests:
         arr.append(scrapeQuora('Physics'))
-    if 'chemistry' in category_string:
+    if 'chemistry' in interests:
         arr.append(scrapeQuora('Chemistry'))
-    if 'math' in category_string:
+    if 'math' in interests:
         arr.append(scrapeQuora('Mathematics'))
-    if 'biology' in category_string:
+    if 'biology' in interests:
         arr.append(scrapeQuora('Biology-1'))
 
-    return arr
-
+    return json.dumps(arr)
 
 @app.route('/search')
 def Search():
@@ -120,8 +136,7 @@ def Search():
     return flask.render_template('index.html',wiki_data=wiki_data,youtube_data=youtube_data,reddit_data=reddit_data,so_data=so_data)
 
 if __name__ == '__main__':
-    scrapeQuora('Physics')
     #print stackoverflow_api("food")
-    #app.debug = True
-    #app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')
 
