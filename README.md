c8750a7219ef6387425b9888c054735b8530e697
sauravtom <stomatrix@gmail.com>
2015-01-24 17:46:41 +0530

    added quoate feed

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-acadsurf
-========
+studdd.in
+=========
 
-+ Academic Search Engine in the making.
-+ Fifth Semester minor project under the guidance of [Dr. Vivek Kumar Aggarwal](http://www.dce.edu/web/Sections/Community/faculty/APPLIEDMATHEMATICS/appliedmathematics.php)
+
--- server.py
+++ server.py
@@ -53,8 +53,8 @@ def scrape_wiki(query):
             pass
     return arr
 
-def stackoverflow_api(query):
-    url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=votes&q=%s&site=math"%query
+def stackoverflow_api(query,site='math'):
+    url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=votes&q=%s&site=%s"%(query,site)
     json = requests.get(url).json()
     return json["items"]
 
@@ -77,24 +77,31 @@ def reddit_api(query):
 def home():
     return flask.render_template('homepage.html')
 
-@app.route('/feed')
+def scrapeQuora(category):
+    url = "http://www.quora.com/%s/rss"%category
+    soup = BeautifulSoup( urllib2.urlopen(url).read() ,'xml' )
+    arr =[]
+    for q in soup.find_all("item"):
+        d['title'] = q.find('title').get_text()
+        d['link'] = q.find('link').get_text()
+        arr.append(d)
+    return arr
+
+@app.route('/feed',methods=['GET'])
 def feed():
-    dict_arr=[]
-    for i in arr:
-        d={}
-        d['title'] = i[1]
-        d['url'] = i[2]
-        d['img'] = i[3]
-        d['chanell'] = fake_chan_name[orig_chan_name.index(i[4])]
-        d['rating'] = i[5]
-        dict_arr.append(d)
+    arr = []
+    categories = request.args.get('categories') or 0
+    category_string = categories.join(',').lower()
+    if 'physics' in category_string:
+        arr.append(scrapeQuora('Physics'))
+    if 'chemistry' in category_string:
+        arr.append(scrapeQuora('Chemistry'))
+    if 'math' in category_string:
+        arr.append(scrapeQuora('Mathematics'))
+    if 'biology' in category_string:
+        arr.append(scrapeQuora('Biology-1'))
 
-    master_dict = {}
-
-    for chanell in fake_chan_name:
-        master_dict[chanell] = [i for i in dict_arr if i['chanell'] == chanell]
-        if not master_dict[chanell]:
-            del master_dict[chanell]
+    return arr
 
 
 @app.route('/search')
@@ -113,7 +120,8 @@ def Search():
     return flask.render_template('index.html',wiki_data=wiki_data,youtube_data=youtube_data,reddit_data=reddit_data,so_data=so_data)
 
 if __name__ == '__main__':
+    scrapeQuora('Physics')
     #print stackoverflow_api("food")
-    app.debug = True
-    app.run(host='0.0.0.0')
+    #app.debug = True
+    #app.run(host='0.0.0.0')
 
