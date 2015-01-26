f2d4952cff7ebf27436050cc297d7286fbf27ad1
Shubham Dokania <skd.1810@gmail.com>
2015-01-26 06:12:25 +0530

    added user login and facebook login

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from settings import parse_credentials
 from parse_rest.connection import register
 from parse_rest.datatypes import Object, GeoPoint
+from parse_rest.user import User
 
 register(parse_credentials["application_id"], parse_credentials["rest_api_key"])
 
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import flask, flask.views
+from flask import session, g, redirect
 app = flask.Flask(__name__)
 import urllib2
-
+from flask_oauth import OAuth
 from parse_rest.connection import register
 from parse_rest.datatypes import Object, GeoPoint
 from models import *
@@ -15,7 +16,7 @@ import grequests
 from dbHelper import VideosDB
 
 CURR_PATH = os.path.dirname(os.path.realpath(__file__))
-conn = sqlite3.connect(CURR_PATH + '/videos.db',check_same_thread=False)
+conn = sqlite3.connect(CURR_PATH + '/videos.db', check_same_thread=False)
 c = conn.cursor()
 
 from scraper import DB,chanell_names
@@ -99,14 +100,14 @@ def reddit_api(query):
         'From': 'stomatrix@gmail.com'  # This is another valid field
     }
     url = "http://www.reddit.com/search.json?q=%s"%query
-    json = requests.get(url,headers=headers).json()
+    json = requests.get(url, headers=headers).json()
     return json["data"]["children"]
 
 
 @app.route('/')
 def home():
     return flask.render_template('homepage.html')
-    
+
 @app.route('/feed',methods=['GET'])
 def feed():
     arr = Feed.Query.all().limit(50)
@@ -133,8 +134,8 @@ def SearchApi():
        'reddit_data':reddit_data,
        'so_data':so_data,
     }, indent=4)
-    
-    
+
+
 @app.route('/search')
 def Search():
     q = request.args.get('q') or 0
@@ -157,23 +158,50 @@ def giveaway_home():
 
 @app.route('/giveaway/upload')
 def giveaway_upload():
-    return flask.render_template('upload.html')    
+    return flask.render_template('upload.html')
 
 @app.route('/giveaway/add',methods=['GET'])
 def giveaway_add():
     post_content = request.args.get('post_content')
     user_id = request.args.get('user_id')
     if 0 < len(post_content) < 140:
-        post = Post(content=post_content, user_id=user_id, is_deleted=0)
+        post = Post(content=post_content, user_id=user_id, is_deleted=0, )
         post.save()
     else:
         pass
         # flash an error message
     return flask.redirect("/giveaway#sa")
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    username = request.form.get('user_name')
+    institution = request.form.get('institute')
+    email = request.form.get('user_email')
+    passwd = request.form.get('user_password')
+    u = User.signup(username=username, password=passwd, NAME=name,  INSTITUTE=institution, email=email)
+    print "==================="
+    return redirect('/')
+
+@app.route('/register')
+def register():
+    return flask.render_template('register.html')
+
 @app.route('/login', methods=['GET'])
 def login():
     return flask.render_template('login.html')
+
+@app.route('/login/submit', methods=['POST'])
+def login_submission():
+    username = request.form.get('user_name')
+    passwd = request.form.get('user_password')
+    u = User.login(username, passwd)
+    arr = Feed.Query.all().limit(50)
+    return flask.render_template('feed.html', u=u, arr=arr)
+
+@app.route('/loggedin', methods=['GET'])
+def loggedin():
+    return flask.render_template('loggedin.html')
 
 
 if __name__ == '__main__':
